4104a5ded2b69d19c4672ecec2400b7626b5b285
acheng2 <41024415+acheng2@users.noreply.github.com>
2023-06-28 16:02:10 -0500

    Update test_traj.py

--- test_traj.py
+++ test_traj.py
@@ -1,45 +1,23 @@
-import copy
-import warnings
-import numpy as np
-
-
-__title__ = "HOPS"
-__author__ = "D. I. G. Bennett, L. Varvelo"
-__version__ = "1.2"
-
 """
-Test1. description
+Trajectories - MESOHOPS.
 """
 
+__version__ = "0.1.0"
 
-###########################################################
-class HopsTrajectory:
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
+
+
+def propagate():
     """
-     HopsTrajectory is the class that a user should interface with to run a single 
-     trajectory calculation. TEST 2 - class
+    Finds a wave function.
+
+    :param kind: Optional "kind" of ingredients wooooo waves.
+    :type kind: list[str] or None - mostly none
+    :raise lumache.InvalidKindError: If the kind is rly  idk invalid.
+    :return: The wave FUNCTION.
+    :rtype: integer
     """
-
-    def __init__(self):
-        """
-        TEST 3.
-    
-        :param kind: Optional "kind" of ingredients.
-        :type kind: list[str] or None
-        :raise lumache.InvalidKindError: If the kind is invalid.
-        :return: The ingredients list.
-        :rtype: list[str]
-        """
-        # Variables for the current state of the system
-
-
-    def initialize(self, psi_0):
-        """
-        TEST 3.
-    
-        :param kind: Optional "kind" of ingredients.
-        :type kind: list[str] or None
-        :raise lumache.InvalidKindError: If the kind is invalid.
-        :return: The ingredients list.
-        :rtype: list[str]
-        """
-        return psi_0
+    return 0
